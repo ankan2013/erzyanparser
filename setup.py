d220a661ebdd30cad3d6c17bf6858558d2c267e0
ankan2013 <ankan2013@gmail.com>
2020-10-27 17:52:14 +0300

    Modify project to be importable as a Python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uniparser-grammar-erzya',
+    version='1.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='timarkh',
+    author_email='',
+    description='',
+    include_package_data=True
+)
